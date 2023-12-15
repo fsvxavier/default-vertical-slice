93ea6ab431613607eb4a97e37d697b6c7ec7540a
fabricio.xavier <fabricio.xavier@dock.tech>
2023-12-15 13:43:25 -0300

    Change remove tests prometheus

--- tests/circuit_braker/client/teste_cb_client.go
+++ tests/circuit_braker/client/teste_cb_client.go
@@ -1,90 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 	"time"
 
-	dto "github.com/prometheus/client_model/go"
-	"github.com/prometheus/common/expfmt"
 	"github.com/sony/gobreaker"
 )
 
 // On client side, we defind a simple function to call the upstream service.
 func DoReq() error {
-	var respBody []byte
 	resp, err := http.Get("http://localhost:8080/metrics")
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	respBody, err = io.ReadAll(resp.Body)
+	_, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-
-	convert, err := prom2json(string(respBody))
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(string(convert))
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		return errors.New("bad response")
 	}
 
 	return nil
-}
-
-func prom2json(str string) ([]byte, error) {
-	parser := &expfmt.TextParser{}
-	families, err := parser.TextToMetricFamilies(strings.NewReader(str))
-	if err != nil {
-		return nil, err
-	}
-
-	out := make(map[string][]map[string]map[string]any)
-
-	for key, val := range families {
-		family := out[key]
-
-		for _, m := range val.GetMetric() {
-			metric := make(map[string]any)
-			for _, label := range m.GetLabel() {
-				metric[label.GetName()] = label.GetValue()
-			}
-			switch val.GetType() {
-			case dto.MetricType_COUNTER:
-				metric["value"] = m.GetCounter().GetValue()
-			case dto.MetricType_GAUGE:
-				metric["value"] = m.GetGauge().GetValue()
-			case dto.MetricType_SUMMARY:
-				metric["value"] = m.GetSummary().GetQuantile()
-			case dto.MetricType_HISTOGRAM:
-				metric["value"] = m.GetHistogram().GetBucket()
-			default:
-				return nil, fmt.Errorf("unsupported type: %v", val.GetType())
-			}
-			family = append(family, map[string]map[string]any{
-				val.GetName(): metric,
-			})
-		}
-
-		out[key] = family
-	}
-
-	output, err := json.Marshal(out)
-	if err != nil {
-		return nil, fmt.Errorf("failed to encode json: %w", err)
-	}
-
-	return output, nil
 }
 
 func DoDb() error {
